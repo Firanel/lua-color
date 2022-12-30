27676c33358957ea7fda34307806b7b082ef710d
Marcel Südmeyer <marcel@suedmeyer.info>
2022-12-30 15:02:27 +0100

    Improve class.lua documentation

--- util/class.lua
+++ util/class.lua
@@ -1,23 +1,14 @@
---
---------------------------------------------------------------------------------
---         File:  class.lua
---
---        Usage:  ./class.lua
---
---  Description:  
---
---      Options:  ---
--- Requirements:  ---
---         Bugs:  ---
---        Notes:  ---
---       Author:  YOUR NAME (), <>
--- Organization:  
---      Version:  1.0
---      Created:  15.04.2021
---     Revision:  ---
---------------------------------------------------------------------------------
---
+-- Code based on:
+-- http://lua-users.org/wiki/SimpleLuaClasses
 
+--- Helper function to create classes
+--
+-- @usage local Color = class(function () --[[ constructor ]] end)
+-- @usage local Color2 = class(
+--   Color,
+--   function () --[[ constructor ]] end,
+--   { prop_a = "some value" }
+-- )
 local function class(base, init, defaults)
   local c = defaults or {}    -- a new class instance
   if not init and type(base) == 'function' then
